1f6bdc3abe18047d0bfc42148a1aed3cbb87d66b
Bach Le <bach@bullno1.com>
2017-08-06 21:09:18 +0800

    Reduce number of iterations in hode_prop

--- test/horde_SUITE.erl
+++ test/horde_SUITE.erl
@@ -37,7 +37,7 @@ no_self_join(_Config) ->
 	horde:stop(Node).
 
 props(_Config) ->
-	?assertEqual([], proper:module(horde_prop, [20000, {to_file, user}])).
+	?assertEqual([], proper:module(horde_prop, [10000, {to_file, user}])).
 
 bootstrap() -> [{timetrap, 15000}].
 
